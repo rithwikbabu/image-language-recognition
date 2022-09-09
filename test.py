1e514b3269f4652fdb0e1bfee15d60be1ad5c77d
Rithwik <rithwikbabu2020@gmail.com>
2022-09-09 15:22:47 -0500

    img basics

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-import imageio as iio
+from PIL import Image
+from os import walk
+
+# Prints Filesnames
+filenames = next(walk("en_sample"), (None, None, []))[2]  # [] if no file
+print(filenames)
+
+# #read the image
+# img = Image.open("en_sample/0.jpg")
+
+# #show images
+# img.show
+
  
-# read an image
-img = iio.imread("g4g.png")
- 
-# write it in a new format
-iio.imwrite("g4g.jpg", img)
