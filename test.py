b3d768207ad4845b79137d031658e8a51f862b07
Rithwik <rithwikbabu2020@gmail.com>
2022-09-26 09:25:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,39 @@
 from PIL import Image
 from os import walk
+import tempfile
 
 # Prints Filesnames
 filenames = next(walk("en_sample"), (None, None, []))[2]  # [] if no file
-print(filenames)
+# print(filenames)
 
-# #read the image
-# img = Image.open("en_sample/0.jpg")
+def set_image_dpi(image, dpiVal):
+    """
+    Rescaling image to 300dpi without resizing
+    :param image: An image
+    :return: A rescaled image
+    """
+    image_resize = image
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
+    temp_filename = temp_file.name
+    image_resize.save(temp_filename, dpi=(dpiVal, dpiVal))
+    return temp_filename
 
-# #show images
+# for name in filenames[0]:
+    #read the image
+name = filenames[0]
+img = Image.open("en_sample/" + name)
+# fetching the dimensions
+print(img)
+wid, hgt = img.size
+print(str(wid) + " x " + str(hgt))
+imgPath = set_image_dpi(img, 30)
+img2 = Image.open(imgPath)
+wid, hgt = img.size
+print(str(wid) + " x " + str(hgt))
+img.show()
+img2.show()
+
+#show images
 # img.show
 
  
